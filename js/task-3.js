9aa2ff526ba57823f5b380025a0d0adebc13fe2d
Anna5725 <ashkliaruk@gmail.com>
2025-03-18 00:55:02 +0300

    index.html, task-1.js, task-2.js, task-3.js, task-4.js, created

---
+++ js/task-3.js
@@ -0,0 +1,27 @@
+function checkForSpam(message){
+const hasSpam = message.toLowerCase().includes('spam');
+const hasSale = message.toLowerCase().includes ('sale');
+if (message = hasSpam||hasSale){
+    return true;
+}
+else{
+    return false;
+}
+}
+
+checkForSpam("Latest technology news");
+checkForSpam("JavaScript weekly newsletter");
+checkForSpam("Get best sale offers now!");
+checkForSpam("Amazing SalE, only tonight!");
+checkForSpam("Trust me, this is not a spam message");
+checkForSpam("Get rid of sPaM emails. Our book in on sale!");
+checkForSpam("[SPAM] How to earn fast money?");
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
