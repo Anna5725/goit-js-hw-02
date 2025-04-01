47ea378b29b93d80484508edf15a65b20aab19f4
Anna5725 <ashkliaruk@gmail.com>
2025-04-02 01:34:33 +0300

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -15,7 +15,6 @@ switch(country){
 default:
     return 'Sorry, there is no delivery to your country';
     }
-
 }
 
 console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
